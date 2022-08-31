654a973023f418eb115c2c4992b9dec3d0df3ed9
SandyLuiza <96505210+sandyluiza@users.noreply.github.com>
2022-08-31 16:45:39 +0000

    Exercicios da aula

--- .ccls-cache/@home@runner@INF006/Revisão_Geral_Ling_C.c
+++ .ccls-cache/@home@runner@INF006/Revisão_Geral_Ling_C.c
@@ -34,6 +34,7 @@
 //   }
 //   printf("Maiores de 50 anos: %d\nMenores de 21 anos: %d\n", maiores, menores); 
 // }
+// OUTRA FORMA DE FAZER:
 // int main()
 // {
 //   int idade = 1, menores=0, maiores=0;
@@ -101,32 +102,31 @@
 //     i++;
 //   }
 // }
-int main()
-{
-  char nomes[20][20], nome[20];
-  int idade[20], i, j;
-  for(i=0;i<20;i++)
-  {
-    printf("Digite o nome: ");
-    fgets(nome, 20, stdin);
-    printf("Digite a idade ");
-    scanf("%d", &idade[i]);
-    getchar();
-    j = nomes[0][i];
-    if(j >= 'L' && j <='S')
-    {
-      strcpy(nomes[i], temp);
-    }
-    i++;
-  }
-}
+// OUTRA FORMA DE FAZER:
+// int main()
+// {
+//   char nomes[20][20], nome[20], j;
+//   int idade[20], i;
+//   for(i=0;i<20;i++)
+//   {
+//     printf("Digite o nome: ");
+//     fgets(nome, 20, stdin);
+//     printf("Digite a idade: ");
+//     scanf("%d", &idade[i]);
+//     getchar();
+//     j = nome[0];
+//     if(j >= 'L' && j <= 'S')
+//     {
+//       strcpy(nomes[i], nome);
+//     }
+//   }
+//   for(i=0;i<20;i++)
+//   {
+//     printf("%s", nomes[i]);
+//   }
+// }
 
-
-
-// Questão 6
-// 6. Armazenar nomes e nota 1, 2, de 15 alunos. calcular e armazenar a media.
-// armazenar tambem a situação do aluno: AP ou RP. imprimir uma listagem
-// contendo nome, notas, media e a situação de cada aluno.
+// Questão 6: Armazenar nomes e nota 1, 2, de 15 alunos. calcular e armazenar a media. armazenar tambem a situação do aluno: AP ou RP. imprimir uma listagem contendo nome, notas, media e a situação de cada aluno.
 // typedef struct{
 //   char nome[20];
 //   double nota1;
@@ -134,9 +134,9 @@ int main()
 //   char situaçao [3];
 // }Aluno;
 // int main(){
-//   Aluno alunos[3];
+//   Aluno alunos[15];
 //   double media;
-//   for(int i = 0; i<3; i++){
+//   for(int i = 0; i<15; i++){
 //     printf("Digite o Nome: ");
 //     gets(alunos[i].nome);
 //     printf("Digite a primeira nota: ");
@@ -148,10 +148,136 @@ int main()
 //     gets(alunos[i].situaçao);
 //     media = (alunos[i].nota1+alunos[i].nota2)/2;
 //   }
-//   for(int i = 0; i<3; i++)
+//   for(int i = 0; i<15; i++)
 //     printf("Nome do aluno: %s\nPrimeira Nota: %.2lf\n"
 //       "Segunda Nota: %.2lf\n"
 //       "Média: %.2lf\nSituação: %s\n"
 //       ,alunos[i].nome, alunos[i].nota1
 //       , alunos[i].nota2, media,alunos[i].situaçao );
 // }
+// OUTRA FORMA DE FAZER:
+// typedef struct
+// {
+//   char nome[20];
+//   float nota1;
+//   float nota2;
+//   char situacao [3];
+//   float media;
+// }
+// Alunos;
+// int main()
+// {
+//   Alunos aluno[15];
+//   for(int i = 0; i<15; i++){
+//     printf("\nDigite o Nome: ");
+//     fgets(aluno[i].nome, 20, stdin);
+//     printf("Digite a primeira nota: ");
+//     scanf("%f", &aluno[i].nota1);
+//     printf("Digite a segunda nota: ");
+//     scanf("%f", &aluno[i].nota2);
+//     getchar();
+//     printf("Digite a situação: ");
+//     fgets(aluno[i].situacao, 3, stdin);
+//     getchar();
+//     aluno[i].media = (aluno[i].nota1+aluno[i].nota2)/2;
+//   }
+//   for(int i = 0; i<15; i++)
+//   {  
+//     printf("\nNome do aluno: %sPrimeira Nota: %f\n"
+//       "Segunda Nota: %f\n"
+//       "Média: %f\nSituação: %s\n"
+//       ,aluno[i].nome, aluno[i].nota1
+//       , aluno[i].nota2, aluno[i].media,aluno[i].situacao);
+//   }
+// }
+
+// Questão 7: Criar programa que leia dois conjuntos de números inteiros, tendo cada um 10 e 20 elementos e apresente os elementos comuns aos conjuntos. lembre-se de que os elementos podem se repetir mas não podem aparecer repetidos na saída
+// void conjuntos_elementos()
+// {
+//   int conj_1[5], conj_2[10], i, j=1, k=1, l=0, m=0, taman=0, inter_1[5], taman_1=0, inter[5];
+//   // lendo as entradas
+//   for (i=0; i<5; i++)
+//   {
+//     printf("Informe o %d elemento do conjunto de 10 ", j);
+//     scanf("%d", &conj_1[i]);
+//     j++;
+//   }
+//   for (i=0; i<10; i++)
+//   {
+//     printf("Informe o %d elemento do conjunto de 20 ", k);
+//     scanf("%d", &conj_2[i]);
+//     k++;
+//   }
+//   // identificando o que aparece nos dois vetores
+//   for (i=0; i<5; i++)
+//   {
+//     for (j=0; j<10; j++)
+//     {
+//       if (conj_1[i] == conj_2[j])
+//       {
+//         inter[taman] = conj_1[i];
+//         taman++;
+//         break;
+//       }
+//     }  
+//   }
+//   // tirando os itens repetidos
+//   #####################################
+//   for (m=0; m<5; m++)
+//   {
+//     for (l=0; l<10; l++)
+//     {
+//       inter_1[taman_1] = conj_1[m];
+//       if (inter_1[taman_1] == conj_1[l])
+//       {
+//         inter_1[taman_1] = ;
+//         taman_1++;
+//         printf("%d",conj_1[m]);
+//         break;
+//       }
+//     }  
+//   }
+//   ##########################################
+//   // imprimir o resultado
+//   for (i=0; i<taman_1;i++)
+//   {
+//     printf("Itens que se repetem: %d\n", inter_1[i]);
+//   }
+// }
+// // Chamando o void
+// int main()
+// {
+//   conjuntos_elementos();
+// }
+
+// Questão 8: Criar programa que leia dados de 20 elementos inteiros. imprimir o maior e o menor, sem ordenar, o percentual de números pares e a media dos elementos do vetor.
+int main()
+{
+  int element[5], i, maior=-9000000000, menor=9000000000000, resto_div;
+  float num_par;
+  double perc_num_par;
+  for (i=0; i<5; i++)
+  {
+    printf("Informe o numero ");
+    scanf("%d", &element[i]);
+    // maior e menor
+    if (element[i] > maior)
+    {
+      maior = element[i];
+    }
+    
+    else if (element[i] < menor)
+    {
+      menor = element[i];
+    }
+    // pares
+    resto_div = element[i]%2;
+    if (resto_div == 0)
+    {
+      num_par++;
+    }
+  }
+  perc_num_par = (num_par/5)*100;
+  printf("maior: %d\nmenor: %d\n", maior, menor);
+  printf("percentual numeros pares: %lf", perc_num_par);
+}
